c46cf31f741d317bc8b1c3e3af499cc2329e2dfe
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2026-05-02 00:37:50 +0530

    server-fixes

--- server/src/app/kafka/kafka.socket-consumer.ts
+++ server/src/app/kafka/kafka.socket-consumer.ts
@@ -1,4 +1,7 @@
+import { db } from "../../db";
 import { kafkaClient } from "./kafka.client";
+import { eq } from "drizzle-orm";
+import { users } from "../../db/schema";
 
 export async function startSocketConsumer(io: {
   emit: (arg0: string, arg1: any) => void;
@@ -14,11 +17,21 @@ export async function startSocketConsumer(io: {
     eachMessage: async ({ message }) => {
       const data = JSON.parse(message.value!.toString());
 
+      const userId = data.userId;
+
+      const user = await db.query.users.findFirst({
+        where: eq(users.id, data.userId),
+      });
+
+      if (!user) return;
+
       io.emit("server:location:update", {
-        userId: data.userId,
+        userId,
         latitude: data.latitude,
         longitude: data.longitude,
         timestamp: data.timestamp,
+        name: user.name,
+        avatar: user.avatar,
       });
     },
   });
